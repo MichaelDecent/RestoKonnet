9de30299e6a04f938b7bb4b845940140afdaf0a6
MichaelDecent <michaeldecent2@gmail.com>
2023-11-08 20:43:08 +0000

    fixed a bug

--- api/v1/views/restaurants.py
+++ api/v1/views/restaurants.py
@@ -35,7 +35,7 @@ def get_vendor_restaurant(vendor_id):
     if not vendor:
         return not_found("vendor does not exist")
 
-    restaurant = [rest.to_dict() for rest in vendor.restaurants]
+    restaurant = vendor.restaurants
     return jsonify(restaurant)
 
 
